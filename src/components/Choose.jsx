95b3c444054081b3e27e14c56c7b8801bca17c9f
DipakDai <dipakkumal673@gmail.com>
2023-06-27 23:36:52 +0545

    choose us added

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Choose = () => {
   return (
-    <div className='choose__us'>
+    <div className='choose__us container-fluid'>
         <p>Choose us</p>
         <h3>Why you choose us to build your career?</h3>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import CardComponent from '../components/CardComponent'
+import Choose from '../components/Choose'
 
 const Home = () => {
   return (
     <div className='container-fluid'>
       <HeroSection />
       <CardComponent />
+      <Choose />
     </div>
   )
 }
