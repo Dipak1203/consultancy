bd462b0203ce9c7a51a25058a68399c411bbd3b7
DipakDai <dipakkumal673@gmail.com>
2023-06-27 20:42:11 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import NavbarComponent from './components/NavbarComponent'
+import './App.css'
+const App = () => {
+  return (
+   <>
+   <NavbarComponent />
+    <Routes>
+      <Route path='' element={<Home />}/>
+    </Routes>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+const NavbarComponent = () => {
+  return (
+    <section className='navbar__container'>
+        <div className='navbar__logo'>
+            <img src='https://moonlightedu.com.np/uploads/company-profile/80_1679988352.png' alt='logo' className='navbar__logo__img w-25' />
+        </div>
+    </section>
+  )
+}
+
+export default NavbarComponent
