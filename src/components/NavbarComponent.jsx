bec30a94b8e3504f474bff24328631aa4a973a19
DipakDai <dipakkumal673@gmail.com>
2023-06-27 21:31:14 +0545

    navbar added

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import '../App.css'
+import { NavLink } from 'react-router-dom'
 const NavbarComponent = () => {
   return (
-    <section className='navbar__container'>
-        <div className='navbar__logo'>
-            <img src='https://moonlightedu.com.np/uploads/company-profile/80_1679988352.png' alt='logo' className='navbar__logo__img w-25' />
+    <section className='navbar__container row py-3'>
+        <div className='navbar__logo col-md-3'>
+            <img src='https://moonlightedu.com.np/uploads/company-profile/80_1679988352.png' alt='logo' className='navbar__logo__img w-100' />
         </div>
+        <nav className='col-md-9'>
+            <ul>
+                <li>
+                    <NavLink>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink>About Us</NavLink>
+                </li>
+                <li>
+                    <NavLink>Study Abroad</NavLink>
+                </li>
+                <li>
+                    <NavLink>language Preparation</NavLink>
+                </li>
+                <li>
+                    <NavLink>Gallery</NavLink>
+                </li>
+                <li>
+                    <NavLink>Contact Us</NavLink>
+                </li>
+                <li>
+                    <button>Inquiry Now!</button>
+                </li>
+            </ul>
+        </nav>
     </section>
   )
 }
