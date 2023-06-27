4b980c1de3a23e2d8fe7ce4833c0c22848ab3d06
DipakDai <dipakkumal673@gmail.com>
2023-06-27 23:55:46 +0545

    added featured card hover

--- src/components/FeaturedBox.jsx
+++ src/components/FeaturedBox.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Container, Card } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledCard = styled(Card)`
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: translateY(-20px);
+  }
+`;
 
 const FeaturedBox = ({ img, title, desc }) => {
   return (
     <Container className='col-md-3 text-center'>
-      <Card className="product shadow-none border-0">
+      <StyledCard className="product shadow-none border-0">
         <Card.Img variant="top" src={img} className="mx-auto" style={{ width: '100px' }} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{desc}</Card.Text>
         </Card.Body>
-      </Card>
+      </StyledCard>
     </Container>
   );
 };
